8e9de2cead5984231b3d578ad4418f348b514d53
Qin Shen <jeanshen@tippett.com>
2010-11-24 12:04:04 -0800

    * Updated "_siteshotgun.sip" to use %Include directive instead of %Import   for "_shotgun.sip". See comments for more details. * Updated "siteshotgun.py" on how to remove the namespaces.

--- example/plugins/python/siteshotgun.py
+++ example/plugins/python/siteshotgun.py
@@ -1,14 +1,15 @@
 ###############################################################################
 # Attempt to make some sense out of the namespaces sip generates
 #
+# -----------------------------------------------------------------------------
 # Before:
-#   import _shotgun
 #   import _siteshotgun
 #   try:
 #       sg = _siteshotgun.SiteSG.Shotgun("...")
-#   except _shotgun.SgError, e:
+#   except _siteshotgun.SgError, e:
 #       print "SgError:", e
 #
+# -----------------------------------------------------------------------------
 # After:
 #   import siteshotgun
 #   try:
@@ -16,56 +17,33 @@
 #   except siteshotgun.SgError, e:
 #        print "SgError:", e
 #
+# OR even simpler:
+#   from siteshotgun import *
+#   try:
+#       sg = Shotgun("...")
+#   except SgError, e:
+#        print "SgError:", e
 #
-# (1) In the case of "_siteshotgun" and "_shotgun" using the same namespace,
-#     SIP implements a C++ namespace as a single Python class - in this case
-#     "_shotgun.SG".
 #
-#     It does not implement it as a separate class in each module. In other
-#     words, in this case, it does not implement it as "_shotgun.SG" AND
-#     "_siteshotgun.SG".
-#
-#     When the "_siteshotgun" module is imported the contents of the SG
-#     namespace that it implements are added to the SG class in the "_shotgun"
-#     module.
-#
-#     Therefore, we need to import "_shotgun" module as well. But only "Part1"
-#     is needed.
-#
-# (2) In our current case, "_siteshotgun" and "_shotgun" use different namespaces,
-#     "SG" and "SiteSG", respectively. If no base class and its derived classe
-#     share the same name and you'd like to remove the namespaces, you'll need to
-#     handle both "_shotgun.SG" and "_siteshotgun.SiteSG", which means both "Part1"
-#     and "Part2" are needed.
-#
-# (3) If "_siteshotgun" and "_shotgun" use different namespaces, and if any
-#     of the base and its derived class share the same name, "Part2" needs
-#     to be done AFTER "Part1" so that the derived class will override its
-#     base class in the globals() dictionary.
+# NOTE: If any of the base class and its derived class share the same name, 
+#       (3) needs to be done AFTER (2) so that the derived class will override 
+#       its base class in the globals() dictionary.
 ###############################################################################
 
-import _shotgun
 import _siteshotgun
 import inspect
 
-######################
-# Part1: _shotgun.SG #
-######################
-for (_sgClassName, _sgClass) in inspect.getmembers(_shotgun, 
-                                                   inspect.isclass):
-    globals()[_sgClassName] = _sgClass
-
-for (_sgClassName, _sgClass) in inspect.getmembers(_shotgun.SG, 
-                                                   inspect.isclass):
-    globals()[_sgClassName] = _sgClass
-
-#############################
-# Part2: _siteshotgun.TipSG #
-#############################
+# (1)
 for (_sgClassName, _sgClass) in inspect.getmembers(_siteshotgun, 
                                                    inspect.isclass):
     globals()[_sgClassName] = _sgClass
 
+# (2)
+for (_sgClassName, _sgClass) in inspect.getmembers(_siteshotgun.SG, 
+                                                   inspect.isclass):
+    globals()[_sgClassName] = _sgClass
+
+# (3)
 for (_sgClassName, _sgClass) in inspect.getmembers(_siteshotgun.SiteSG, 
                                                    inspect.isclass):
     globals()[_sgClassName] = _sgClass
@@ -74,7 +52,6 @@ for (_sgClassName, _sgClass) in inspect.getmembers(_siteshotgun.SiteSG,
 del _sgClassName
 del _sgClass
 del inspect
-del _shotgun
 del _siteshotgun
 
 ###############################################################################
